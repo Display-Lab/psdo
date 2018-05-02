21471a1880a16046b4e374d688fd359c4eb3cfe6
CooperStansbury <cstansbu@umich.edu>
2018-05-02 15:14:49 -0400

    add tool to extract 10 most recent FIO ids for quickly checking next fio ID values

---
+++ tools/fio_ids.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Requires xmllint to be installed
+command -v robot 1> /dev/null 2>&1 || \
+  { echo >&2 "robot required but it's not installed.  Aborting."; exit 1; }
+
+# Start by assuming it was the path invoked.
+THIS_SCRIPT="$0"
+
+# Handle resolving symlinks to this script.
+# Using ls instead of readlink, because bsd and gnu flavors
+# have different behavior.
+while [ -h "$THIS_SCRIPT" ] ; do
+  ls=`ls -ld "$THIS_SCRIPT"`
+  # Drop everything prior to ->
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    THIS_SCRIPT="$link"
+  else
+    THIS_SCRIPT=`dirname "$THIS_SCRIPT"`/"$link"
+  fi
+done
+
+# Get path to the scripts directory.
+SCRIPT_DIR=$(dirname "${THIS_SCRIPT}")
+
+FIO_FILE="${SCRIPT_DIR}/../src/ontology/fio-edit.owl"
+SPARQL_FILE="${SCRIPT_DIR}/ids.sparql"
+CSV_FILE="${SCRIPT_DIR}/../src/ontology/id.csv"
+
+# Extract FIO Ids
+echo "Using ROBOT to extract 10 recent FIO Ids (largest)"
+robot query --input ${FIO_FILE} --query ${SPARQL_FILE} ${CSV_FILE}
+
+# Print query results to the command line
+echo "$(<${CSV_FILE})"
+# sed -n '1,11p' ${CSV_FILE}
