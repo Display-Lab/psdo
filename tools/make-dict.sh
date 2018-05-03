3b31075f97f7090d0b7d368f06e79c5c0c62d969
CooperStansbury <cstansbu@umich.edu>
2018-05-03 09:43:29 -0400

    add spek terms from issue 138 create basic structure needed for KB prototype
    fix readme and dictionary formatting
    improve readability and refresh after new term additions
    
    remove temp csv file and update .gitignore
    keep remote repo clean

--- tools/make-dict.sh
+++ tools/make-dict.sh
@@ -39,4 +39,5 @@ robot query --input ${FIO_FILE} --query ${SPARQL_FILE} ${CSV_FILE}
 # Format csv to markdown
 echo "Formatting dictionary markdown"
 tail -n +2 ${CSV_FILE} | sort | sed -e 's|^\([^,]*\),\([^,]*\),\(.*\)|## \1\
-\3\2|' > ${MD_FILE}
+\3\
+\2|' > ${MD_FILE}
