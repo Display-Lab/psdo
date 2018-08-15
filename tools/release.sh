1fd79f5da4297cf933923d2289a1bcb2eef8ac7e
CooperStansbury <cstansbu@umich.edu>
2018-08-15 15:47:54 -0400

    stub out release.sh script move towards a stable release process

---
+++ tools/release.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Requires xmllint to be installed
+command -v robot 1> /dev/null 2>&1 || \
+  { echo >&2 "robot required but it's not installed.  Aborting."; exit 1; }
+
+# Start by assuming it was the path invoked.
+THIS_SCRIPT="$0"
+
+# Handle resolving symlinks to this script.
+# Using ls instead of readlink, because bsd and gnu flavors
+# have different behavior.
+while [ -h "$THIS_SCRIPT" ] ; do
+  ls=`ls -ld "$THIS_SCRIPT"`
+  # Drop everything prior to ->
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    THIS_SCRIPT="$link"
+  else
+    THIS_SCRIPT=`dirname "$THIS_SCRIPT"`/"$link"
+  fi
+done
+
+# Get path to the scripts directory.
+SCRIPT_DIR=$(dirname "${THIS_SCRIPT}")
+
+PSDO="${SCRIPT_DIR}/../psdo.owl"
+
+# add date to IRI version
+robot annotate \
+  --input ${PSDO} \
+  --ontology-iri "http://purl.obolibrary.org/obo/psdo.owl" \
+  --version-iri "http://purl.obolibrary.org/obo/`date '+%Y-%m-%d'`/psdo.owl" \
+  --output psdo-updated.owl
+
+# clean up
+rm psdo.owl
+mv psdo-updated.owl psdo.owl
